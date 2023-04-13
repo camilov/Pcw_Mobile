ded0000dbb1c44fadba82b89ebd9b24465874be2
cristian <cagudelovillegas@gmail.com>
2023-04-13 16:46:00 -0500

    Avance en abonos

--- app/src/main/java/com/example/pcw/AbonosClass/AbonosActivity.kt
+++ app/src/main/java/com/example/pcw/AbonosClass/AbonosActivity.kt
@@ -1,14 +1,20 @@
 package com.example.pcw.AbonosClass
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.TableLayout
+import android.widget.TableRow
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.pcw.Api.ApiService
 import com.example.pcw.Api.ServiceBuilder
+import com.example.pcw.ClienteSendDataResponse
+import com.example.pcw.ClienteSendResponse
+import com.example.pcw.ClientesClass.ClientesActivity
 import com.example.pcw.DataResponse.AbonoItemResponse
 import com.example.pcw.DataResponse.AbonosDataResponse
 import com.example.pcw.R
@@ -16,13 +22,21 @@ import com.example.pcw.databinding.ItemAbonosBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import org.w3c.dom.Text
+import retrofit2.Call
+import retrofit2.Callback
 import retrofit2.Response
+import java.time.Instant.now
+import java.time.LocalDate
+import java.util.*
 
 class AbonosActivity : AppCompatActivity() {
 
     private lateinit var binding: ItemAbonosBinding
     private lateinit var servicio: ApiService
     private var tlAbonos: TableLayout?=null
+    private  var trAbono: TableRow?=null
+    private var selectedRow: TableRow? = null
 
     private var idTarjeta: Number? = null
     private var idCliente: Number? = null
@@ -37,9 +51,90 @@ class AbonosActivity : AppCompatActivity() {
 
         servicio =  ServiceBuilder.buildService(ApiService::class.java)
         initUI()
+        initListeners()
 
         Log.d("scord1", "${idTarjeta}")
     }
+
+    private fun initListeners() {
+
+        binding.btnAddAbono.setOnClickListener{addAbono()}
+
+        }
+
+    private fun addAbono() {
+
+        val currentNumCuota   = binding.etNumCuotaAbonos.text.toString()
+        val currentValorAbono = binding.etValorAbonoAbonos.text.toString()
+        val current = Date()
+
+        if(currentNumCuota != null && currentValorAbono != null){
+
+            CoroutineScope(Dispatchers.IO).launch {
+                // val myResponse:ApiService = ServiceBuilder.buildService(ApiService::class.java)
+                Log.d("scordsito","$idTarjeta")
+                val abonoData = AbonoItemResponse(ClientesActivity.CREATE_ID,idTarjeta!!,currentNumCuota.toInt(),currentValorAbono.toFloat(),current)
+
+                servicio.addAbono(abonoData).enqueue(
+                    object: Callback<AbonoItemResponse> {
+                        override fun onResponse(
+                            call: Call<AbonoItemResponse>,
+                            response: Response<AbonoItemResponse>
+                        ) {
+                            Toast.makeText(this@AbonosActivity,"Se Añadio abono correctamente",Toast.LENGTH_LONG)
+                        }
+
+                        override fun onFailure(call: Call<AbonoItemResponse>, t: Throwable) {
+                            Toast.makeText(this@AbonosActivity,"Error",Toast.LENGTH_LONG)
+                        }
+
+                    }
+                )
+
+            }
+
+        }
+
+
+    }
+
+    private fun selectedRow() {
+
+        val tableLayout = findViewById<TableLayout>(R.id.tlAbonos)
+
+        for (i in 0 until tableLayout.childCount) {
+            val tableRow = tableLayout.getChildAt(i) as TableRow
+
+            tableRow.setOnClickListener(object : View.OnClickListener {
+                override fun onClick(view: View) {
+                    // Deseleccionar la fila anteriormente seleccionada
+                    selectedRow?.setBackgroundColor(Color.WHITE)
+
+                    // Guardar la fila seleccionada actualmente
+                    selectedRow = tableRow
+
+                    // Cambiar el color de fondo de la fila seleccionada
+                    tableRow.setBackgroundColor(Color.GRAY)
+                   // var tb = view as TableRow
+                    var abono = tableRow.getChildAt(0) as TextView
+                    var abonoCodigo = abono.text.toString()
+
+                    var cuota = tableRow.getChildAt(1) as TextView
+                    var cuoutaCodigo = cuota.text.toString()
+
+                    var valor = tableRow.getChildAt(2) as TextView
+                    var valorCodigo = valor.text.toString()
+
+                   // Log.d("scordsito","$controlCodigo")
+                    binding.etNumCuotaAbonos.setText( cuoutaCodigo)
+                    binding.etValorAbonoAbonos.setText( valorCodigo)
+
+
+                }
+            })
+        }
+    }
+
 
     private fun fillTable(abonosList: List<AbonoItemResponse>) {
 
@@ -49,24 +144,21 @@ class AbonosActivity : AppCompatActivity() {
 
 
             val registro = LayoutInflater.from(this).inflate(R.layout.row_table_abonos,null,false)
-            val tvIdTarjeta = registro.findViewById<View>(R.id.tvIdtarjeta) as TextView
+            val tvIdAbono = registro.findViewById<View>(R.id.tvIdAbono) as TextView
             val tvNumCuota = registro.findViewById<View>(R.id.tvNumCuota) as TextView
             val tvValorAbono = registro.findViewById<View>(R.id.tvValorAbono) as TextView
 
-            println(abonosLists.idTarjeta)
-            println(abonosLists.numCuota)
-            println(abonosLists.valorAbono)
-
-            tvIdTarjeta.text = abonosLists.idTarjeta.toString()
+            tvIdAbono.text = abonosLists.idAbono.toString()
             tvNumCuota.text = abonosLists.numCuota.toString()
-            tvValorAbono.text = abonosLists.valorAbono.toString()
+            tvValorAbono.text = abonosLists.valorAbono.toInt().toString()
 
-            //tlAbonos?.addView(registro)
             binding.tlAbonos.addView(registro)
 
         }
 
     }
+
+
 
     private fun initUI() {
 
@@ -86,6 +178,9 @@ class AbonosActivity : AppCompatActivity() {
                     runOnUiThread {
                        // adapterClienteTarjeta.updateList(response.Lista)
                         fillTable(response.Lista)
+                        selectedRow()
+                        ////////////////////////
+
                         /*binding.progressBar.isVisible = false*/ }
                     }
                 } else {
@@ -94,3 +189,6 @@ class AbonosActivity : AppCompatActivity() {
         }
     }
 }
+
+
+
--- app/src/main/java/com/example/pcw/Api/ApiService.kt
+++ app/src/main/java/com/example/pcw/Api/ApiService.kt
@@ -1,6 +1,7 @@
 package com.example.pcw.Api
 
 import com.example.pcw.*
+import com.example.pcw.DataResponse.AbonoItemResponse
 import com.example.pcw.DataResponse.AbonosDataResponse
 import com.example.pcw.DataResponse.TarjetasDataResponse
 import retrofit2.Call
@@ -10,28 +11,33 @@ import retrofit2.http.*
 interface ApiService {
 
     /************************  RUTAS TARJETAS  *****************/
+    /** Consultar cliente por nombre**/
     @GET("/api/clientes/{nombre}")
     suspend fun getClienteDetail(@Path("nombre") clienteId:String):Response<ClientesDataResponse>
 
-
+    /** Crear cliente**/
     @POST("/api/create_clientes/")
     fun addUser(@Body userData: ClienteSendDataResponse): Call<ClienteSendResponse>
 
+    /** Modificar cliente**/
     @PUT("/api/modificar_Cliente/{idCliente}")
     fun modifyClient(@Path("idCliente") clienteId:Int,
                      @Body userData: ClienteModifyResponse
     ): Call<ClienteSendModifyResponse>
 
     /**********************  RUTAS TARJETAS  **********************/
+    /** Consultar tarjetas por id de cliente**/
     @GET("/api/tarjetas_id/{idCliente}")
     suspend fun getClientTarjetaDetail(@Path ("idCliente") tarjetaId: Number?): Response<TarjetasDataResponse>
 
 
     /**********************  RUTAS ABONOS  **********************/
+    /** Consultar abonos por id de tarjeta**/
     @GET("/api/abonos/{idTarjeta}")
     suspend fun getAbonosDetail(@Path ("idTarjeta") tarjetaId: Number?): Response<AbonosDataResponse>
 
-
-
+    /** Crear abono**/
+    @POST("/api/create_abono/")
+    fun addAbono(@Body abonoData: AbonoItemResponse): Call<AbonoItemResponse>
 
 }
